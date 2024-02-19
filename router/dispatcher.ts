f3fc3c884c8c824bff94ac0519d290a6a12e52d0
1 <a1@1s-MacBook-Air.local>
2024-02-19 14:32:00 +0330

    git fix controllers url from diapatcher

--- router/dispatcher.ts
+++ router/dispatcher.ts
@@ -5,7 +5,7 @@ const controllers: ControllersType = {};
 
 fs.readdirSync('./controllers').forEach((file) => {
     const filePath = path.join('controllers', file);
-    const module = require('./' + filePath).default;
+    const module = require('../' + filePath).default;
     const moduleName = path.basename(filePath, path.extname(file));
     controllers[moduleName] = module;
 
